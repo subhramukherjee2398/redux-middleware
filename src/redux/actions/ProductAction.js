b3b0d31b295c4b9491a55fd71502899a57de5ec0
subhramukherjee2398 <subhramukherjee560@gmail.com>
2022-03-15 07:14:35 +0530

    middleware setup

--- src/redux/actions/ProductAction.js
+++ src/redux/actions/ProductAction.js
@@ -1,4 +1,17 @@
+import StoreApi from "../../apis/StoreApi";
 import { ActionTypes } from "../constants/action-types";
+
+export const fetchProduct = () => {
+  return async function (dispatch, getState) {
+    const response = await StoreApi.get("/products");
+    console.log({ response });
+    dispatch({
+      type: ActionTypes.FETCH_PRODUCTS,
+      payload: response.data,
+    });
+  };
+};
+
 export const setProducts = (products) => {
   return {
     type: ActionTypes.SET_PRODUCTS,
